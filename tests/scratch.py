a07d2b9c1c266ee3a9b79dfd471fdd70967bbd35
Avinash Pujala <avinashpujala@gmail.com>
2020-06-03 12:04:13 -0400

    multisensory by Andrew Owens and Nussl by North Western's Audiolab now added as submodules

--- tests/scratch.py
+++ tests/scratch.py
@@ -13,16 +13,17 @@ ydl_opts = {'format': '22/18',
 fileName = '/home/avi/Documents/blah.mp4'
 startTime=1
 endTime=3
-vcodec = 'libx264' # ('libx264', 'rawvideo')
-acodec = 'aac' #('aac', 'pcm')
+vcodec = 'vp9' # ('libx264', 'rawvideo', 'vp9')
+acodec = 'libvorbis' #('aac', 'pcm', 'libvorbis')
 
 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
     download_url = ydl.extract_info(url=yt_url, download=False)['url']
 
 ffin = ffmpeg.input(download_url, ss=startTime, to=endTime)
-# ffout=ffin.output(fileName, format='mp4', r=25, vcodec=vcodec,
-#                   crf=18, preset='veryfast', pix_fmt='yuv420p', acodec=acodec,
-#                   audio_bitrate=128000, strict='experimental').global_args('-y')
-ffout=ffin.output(fileName, format='mp4').global_args('-y')
-# ffout = ffout.global_args('-loglevel', 'error')
+ffout=ffin.output(fileName, format='mp4', r=25, vcodec=vcodec,
+                  crf=18, preset='veryfast', pix_fmt='yuv420p', acodec=acodec,
+                  audio_bitrate=128000, strict='experimental').global_args('-y')
+# ffout=ffin.output(fileName, format='mp4')
+# ffout=ffin.output(fileName, format='mp4').global_args('-y')
+ffout = ffout.global_args('-loglevel', 'error')
 # ffout.run()
