d164db998a3f0b279bccabc1d35f14086a2ff0e0
Ubuntu <ubuntu@ip-172-31-48-5.ec2.internal>
2020-06-15 04:43:57 +0000

    Audio-segmentation-from-video network pushed

--- test.py
+++ test.py
@@ -1,40 +1,16 @@
-# import glob
-# from os.path import join
-# import processMedia
-# import numpy as np
-# import importlib
-# importlib.reload(processMedia)
-# import matplotlib.pyplot as plt
-#
-# idx = 0
-# dir_vids = r'C:\Users\Avi\Google Drive\Code\projects\av_segmentation\data\avspeech'
-# filePaths = glob.glob(join(dir_vids, '*.mp4'))
-# fp = filePaths[idx]
-# # fp = join(dir_vids, '_-_1K9uCid4_270.067000_275.767000.mp4')
-# aud, vid = processMedia.separate_streams(fp)
-# imgs_sub = vid.imgs[:, ::2, ::2]
-# dImgs = np.gradient(imgs_sub, axis=0).astype(imgs_sub.dtype)
-# dImgs2 = np.gradient(imgs_sub-imgs_sub.mean(), axis=0).astype(imgs_sub.dtype)
-# foo = dImgs.mean(axis=-1).mean(axis=-1)
-# foo2 = dImgs2.mean(axis=-1).mean(axis=-1)
-#
-# #%%
-# t_aud = np.linspace(0, aud.dur, len(aud.ts))
-# t_vid = np.linspace(0, vid.dur, vid.imgs.shape[0])
-# plt.plot(t_aud, aud.ts/aud.ts.max(), label='Audio')
-# plt.plot(t_vid, foo/foo.max(), label='Diff only')
-# plt.plot(t_vid, foo2/foo2.max(), ls ='--', label='Mean sub + diff')
-# plt.legend()
-#
-# #%%
-# # processMedia.to_videoClip(dImgs, vid.fps).write_videofile('dMovie.mp4')
-# # processMedia.to_videoClip(dImgs2, vid.fps).write_videofile('dMovie2.mp4')
-#
-#
-# """
-# # yt_url = r'https://www.youtube.com/watch?v=gMz4r5RLvKs'
-# # yt_url = r'https://www.youtube.com/watch?v=fregObNcHC8'
-# # yt_url = yt_url.split('?v=')[-1]
-# """
+import pandas as pd
+import glob
+import os
+import downloadMedia
 
-print(np.arange(10))
+
+dir_csv = '/home/ubuntu/avinash/code/projects/av_segmentation/data_csv'
+dir_out = r'/home/ubuntu/avinash/vol_ebs/audioset'
+
+path_csv = os.path.join(dir_csv, 'balanced_train_segments.csv')
+
+
+df_as = pd.read_csv(os.path.join(dir_csv, 'balanced_train_segments.csv'), header=None,
+                    names=['yt_id', 'start_time', 'stop_time', 'label'])
+
+# downloadMedia.download_av_speech(path_csv, dir_out, n_files=len(df_as))
